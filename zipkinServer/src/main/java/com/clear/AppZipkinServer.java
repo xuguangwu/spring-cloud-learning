953eac245db18e9cee542382c7aa9bda10ca4736
xuguangwu <xuguangwu24@163.com>
2019-04-25 22:31:48 +0800

    update

--- zipkinServer/src/main/java/com/clear/AppZipkinServer.java
+++ zipkinServer/src/main/java/com/clear/AppZipkinServer.java
@@ -2,12 +2,14 @@ package com.clear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 @EnableZipkinServer
 @SpringBootApplication
-public class AppSleuth {
+@EnableEurekaClient
+public class AppZipkinServer {
     public static void main(String[] args) {
-        SpringApplication.run(AppSleuth.class);
+        SpringApplication.run(AppZipkinServer.class);
     }
 }
